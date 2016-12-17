fc0e354225836fadd14f5522be33b319c1823692
Nicu Timofte <timofte.alexandru05@yahoo.com>
2016-12-17 14:10:23 +0300

    designed service to return a Promise and get data from the Promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -76,13 +76,16 @@ export class AppComponent implements OnInit  {
   selectedHero: Hero;
 
   constructor(private heroService: HeroService){}
+  ngOnInit():void{
+      this.getHeroes();
+    }
 
   onSelect(hero: Hero): void{
     this.selectedHero=hero;
   }
 
   getHeroes(): void{
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().then(heroes => this.heroes=heroes);    
   }
 }
 
