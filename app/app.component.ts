30ad792072a1a705dcd7e0493268c6ebed54148c
Nicu Timofte <timofte.alexandru05@yahoo.com>
2016-12-17 14:02:12 +0300

    implemented lifecycle hook ngOnInit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import {HeroDetailComponent} from './hero-detail.component';
 import {Hero} from './hero';
 import { HeroService } from './hero.service';
@@ -70,7 +70,7 @@ import { HeroService } from './hero.service';
   `
 
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   title  = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
@@ -87,3 +87,4 @@ export class AppComponent  {
 }
 
 
+ 
