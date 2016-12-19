7acd1c96057ca0dcff03cd93eed9988e1b101071
Nicu Timofte <timofte.alexandru05@yahoo.com>
2016-12-19 22:06:42 +0300

    added dashboard comp.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,22 +1,42 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { AppComponent }  from './app.component';
-import {HeroDetailComponent} from './hero-detail.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+
 
 @NgModule({
   imports:[
   	BrowserModule, 
-  	FormsModule 
+  	FormsModule,
+  	RouterModule.forRoot([
+		{
+			path: 'heroes',
+			component: HeroesComponent
+		},
+		{
+			path: 'dashboard',
+			component: DashboardComponent
+		}
+	])
   ],
   declarations:[
   	AppComponent,
-  	HeroDetailComponent
+  	DashboardComponent,
+  	HeroDetailComponent,
+  	HeroesComponent
+  ],
+  providers:[
+  	HeroService
   ],
   bootstrap:[ 
   	AppComponent 
-  ],
-  providers: [HeroService]
+  ]
 })
+
 export class AppModule { }
---
+++ app/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-dashboard',
+	template: '<h3>My DashBoard</h3>'
+})
+
+export class DashboardComponent { }
