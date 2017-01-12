e32bbcf2d2a6e789f4eb44e5cea17fc7349c55d4
Timofte Alexandru <timofte.alexandru05@yahoo.com>
2017-01-12 11:41:50 +0200

    created in memory api

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,17 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
 
 @Injectable()
 
 export class HeroService{
+	private heroesUrl = 'api/heroes';
+	private headers = new Headers({'Content-Type': 'application/json'});
+
+	constructor(private http: Http){
+
+	}
 	getHero(id: number): Promise<Hero>{
-		return this.getHeroes()
-			.then(heroes => heroes.find(hero => hero.id === id));
+		const url = `${this.heroesUrl}/${id}`;
+		return this.http.get(url)
+			.toPromise()
+			.then(response => response.json().data as Hero )
+			.catch(this.handleError);
 	}
 
 	getHeroes(): Promise<Hero[]>{
-		return Promise.resolve(HEROES);
+		return this.http.get(this.heroesUrl)
+			.toPromise()
+			.then(response => response.json().data as Hero[])
+			.catch(this.handleError);
+	}
+
+	private handleError(error: any):Promise<any>{
+		console.error('An error occured', error);
+		return Promise.reject(error.message || error);
 	}
 
 	getHeroesSlowly(): Promise<Hero[]>{
@@ -19,4 +39,14 @@ export class HeroService{
 			resolve => setTimeout(resolve,2000))
 			.then(()=>this.getHeroes());
 	}
+
+	update(hero: Hero): Promise<Hero> {
+		const url = `${this.heroesUrl}/${hero.id}`;
+		return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+	}
+
 }
