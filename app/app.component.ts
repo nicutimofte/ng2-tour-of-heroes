b663c0f0224f0abd1721b6e9574943ac4dc8b683
Timofte Alexandru <timofte.alexandru05@yahoo.com>
2017-01-12 11:13:40 +0200

    added routerLink and code cleaning

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
+	moduleId: module.id,
 	selector: 'my-app',
-	template: `
-<h1>{{title}}</h1>		
-<a routerLink="/heroes">Heroes</a>
-<router-outlet></router-outlet>
-`
+	styleUrls:['app.component.css'],
+	templateUrl: 'app.component.html'
 })
 export class AppComponent {
 title = 'Tour of Heroes';
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,9 +5,15 @@ import { Hero } from './hero';
 @Injectable()
 
 export class HeroService{
+	getHero(id: number): Promise<Hero>{
+		return this.getHeroes()
+			.then(heroes => heroes.find(hero => hero.id === id));
+	}
+
 	getHeroes(): Promise<Hero[]>{
 		return Promise.resolve(HEROES);
 	}
+
 	getHeroesSlowly(): Promise<Hero[]>{
 		return new Promise<Hero[]>(
 			resolve => setTimeout(resolve,2000))
